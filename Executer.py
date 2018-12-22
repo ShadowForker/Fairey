ab9fed9178d067357b1a9370ff5aa3d6e51abf2d
unknown <nitzanehadar@gmail.com>
2018-12-22 20:41:34 +0200

    minor bug fixes

--- Executer.py
+++ Executer.py
@@ -2,8 +2,8 @@ import whois
 import pandas as pd
 
 def MainExecuter(urlList):
-    result=pd.DataFrame()
-    for url in urlList:
+    result=pd.DataFrame.from_dict(getWhois(urlList[0]))
+    for url in urlList[1:]:
         whoisInfo=getWhois(url)
         result.join(pd.DataFrame(whoisInfo,index=result.index))
     print (result)
